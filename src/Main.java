f82a3014a45782fbbd80ab02fdf5039881f5047b
hw commit <demaniks@yahoo.com>
2022-11-02 19:49:02 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int clientOS = 1;
-        if (clientOS == 0) {
+        int clientDeviceYear = 2015;
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
-        } else if (clientOS == 1) {
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println( "Установите версию приложения для Android по ссылке.");
+        }
+        //task 2
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
     }
 
